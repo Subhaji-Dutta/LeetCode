8b7cfb7af4fdf159ee138dd390f593f78745ddc5
Subhajit-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2026-04-26 16:29:53 +0530

    Add files via upload

---
+++ RotateArray.py
@@ -0,0 +1,41 @@
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+
+# Solve
+
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        k %= n  # handle k > n
+
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        # 3-step reversal
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
+
+def rotate(self, nums, k):
+    k %= len(nums)
+    nums[:] = nums[-k:] + nums[:-k]
