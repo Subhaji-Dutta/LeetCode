fce4992475d701bc92c5f1d1f7ce5dd7ec4fd05c
Subhajit-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2026-07-07 10:32:18 +0530

    Time: 146 ms (68.2%), Space: 94.9 MB (29.52%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -1,21 +1,15 @@
-from collections import deque
-
 class Solution:
     def minDepth(self, root):
         if not root:
             return 0
 
-        queue = deque([(root, 1)])   # (node, current depth)
+        if not root.left:
+            return self.minDepth(root.right) + 1
 
-        while queue:
-            node, depth = queue.popleft()
+        if not root.right:
+            return self.minDepth(root.left) + 1
 
-            # First leaf found
-            if not node.left and not node.right:
-                return depth
-
-            if node.left:
-                queue.append((node.left, depth + 1))
-
-            if node.right:
-                queue.append((node.right, depth + 1))
+        return min(
+            self.minDepth(root.left),
+            self.minDepth(root.right)
+        ) + 1
