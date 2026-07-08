aa15dfe0f6912d19dca51d15158634b6608456ca
Subhajit-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2026-07-08 21:43:44 +0530

    Time: 10 ms (57.68%), Space: 18.6 MB (52.43%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -1,25 +1,23 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution(object):
     def findTarget(self, root, k):
-        if not root:
-            return False
-
         seen = set()
-        queue = deque([root])
 
-        while queue:
-            node = queue.popleft()
+        def f(root):
+            if not root:
+                return False
 
-            if k - node.val in seen:
+            if k - root.val in seen:
                 return True
 
-            seen.add(node.val)
+            seen.add(root.val)
 
-            if node.left:
-                queue.append(node.left)
+            return f(root.left) or f(root.right)
 
-            if node.right:
-                queue.append(node.right)
-
-        return False
+        return f(root)
